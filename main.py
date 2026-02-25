7e9672493d11f6a117480a49be8f1fffd3198ad3
Reeni <reenicawaling12@gmail.com>
2026-02-25 13:44:39 +0400

    Add technical dashboard: Volume, MACD panels with mode toggle!

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ def serve_frontend():
 def predict(ticker: str):
     print("Fetching data for: ", ticker)
 
-    data = yf.download(ticker, period="1mo", progress=False)
+    data = yf.download(ticker, period="3mo", progress=False)
     print("Data fetched!")
 
     if data.empty:
         return{"error": "Invalid ticker"}
     
     close_prices = data["Close"]
+    volume = data["Volume"]
+    if hasattr(volume, "columns"):
+        volume = volume.iloc[:,0]
+    volume_list = volume.fillna(0).tolist()
 
     #for handling multi-index cases safely
     if hasattr(close_prices, "columns"):
@@ -47,15 +51,58 @@ def predict(ticker: str):
         for x in ma21
     ]
 
+    delta = close_prices.diff()
+
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+
+    avg_loss = avg_loss.replace(0, 1e-10)
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+
+    rsi = rsi.replace([float("inf"), float("-inf")], None)
+    rsi = rsi.where(rsi.notna(), None)
+    rsi_list = [
+        None if (x is None or not isinstance(x, (int, float))) else float(x)
+        for x in rsi
+    ]
+
+    def safe_float(value):
+        if value is None:
+            return None
+        if isinstance(value,float):
+            if math.isnan(value) or math.isinf(value):
+                return None
+        return float(value)
+    
+    ema12 = close_prices.ewm(span=12, adjust=False).mean()
+    ema26 = close_prices.ewm(span=26, adjust=False).mean()
+    macd = ema12 - ema26
+    signal = macd.ewm(span=9, adjust=False).mean()
+    histogram = macd - signal
+
+    macd_list = [safe_float(x) for x in macd]
+    signal_list = [safe_float(x) for x in signal]
+    hist_list = [safe_float(x) for x in histogram]
+
     return{
         "ticker": ticker.upper(),
-        "latest_price": round(float(latest_price),2),
+        "latest_price": safe_float(round(float(latest_price),2)),
         "dates": dates,
-        "datasets":{
-            "prices": prices,
-            "ma7": ma7_list,
-            "ma21" :ma21_list,
+        "datasets": {
+            "prices": [safe_float(x) for x in prices],
+            "ma7": [safe_float(x) for x in ma7_list],
+            "ma21": [safe_float(x) for x in ma21_list],
+            "rsi": [safe_float(x) for x in rsi_list],
+            "volume": [safe_float(x) for x in volume_list],
+            "macd": macd_list,
+            "macd_signal": signal_list,
+            "macd_hist": hist_list
         },
-        "change": round(float(change),2),
-        "percent_change": round(float(percent_change),2) 
+        "change": safe_float(round(float(change),2)),
+        "percent_change": safe_float(round(float(percent_change),2)),
     }
--- static/script.js
+++ static/script.js
@@ -1,4 +1,6 @@
 let chart;
+let rsiChart;
+let macdChart;
 
 function searchStock(){
     let ticker = document.getElementById("tickerInput").value;
@@ -14,22 +16,44 @@ function searchStock(){
             document.getElementById("result").innerHTML = data.error;
             return;
         }
-        document.getElementById("result").innerHTML =
-            "Latest price for " + data.ticker + " is $" + data.latest_price;
+        const priceElement = document.getElementById("priceText");
+        const changeElement = document.getElementById("changeText");
+
+        priceElement.innerHTML = `${data.ticker} $${data.latest_price}`;
+
+        const change = data.change;
+        const percent = data.percent_change;
+    
+        if (change >=0){
+            changeElement.innerHTML =  ` ▲ +${change} (${percent.toFixed(2)}%)`;
+            changeElement.className = "change positive";
+        } else {
+            changeElement.innerHTML =  ` ▼ +${change} (${percent.toFixed(2)}%)`;
+            changeElement.className = "change negative";
+        }
         renderChart(
             data.dates, 
             data.datasets.prices,
             data.datasets.ma7,
             data.datasets.ma21,
+            data.datasets.rsi,
+            data.datasets.volume,
             data.ticker
+        );
+        renderMACD(
+            data.dates,
+            data.datasets.macd,
+            data.datasets.macd_signal,
+            data.datasets.macd_hist
         );
     })
     .catch(error => {
         console.error("Error", error);
     });
+    
 }
 
-function renderChart(dates,prices,ma7,ma21,ticker){
+function renderChart(dates,prices,ma7,ma21,rsi,volume,ticker){
     console.log(dates);
     console.log(prices);
     const ctx = document.getElementById("stockChart").getContext("2d");
@@ -46,6 +70,14 @@ function renderChart(dates,prices,ma7,ma21,ticker){
         data:{
             labels: dates,
             datasets: [{
+                type: "bar",
+                label: "Volume",
+                data: volume,
+                yAxisID: "yVolume",
+                backgroundColor: "rgba(0,255,204,0.15)",
+                borderWidth:0
+            },
+            {
                 label: ticker + " Price",
                 data: prices,
                 borderColor: "#00ffcc",
@@ -54,12 +86,45 @@ function renderChart(dates,prices,ma7,ma21,ticker){
                 borderwidth: 3,
                 tension: 0.4, //smooth curves
                 pointRadius: 0
-            }]
+            },
+            {
+                label: "MA 7",
+                data: ma7,
+                borderColor: "#FFD700",
+                borderWidth: 2,
+                tension: 0.4,
+                pointRadius: 0,
+                fill: false
+            },
+            {
+                label: "MA 21",
+                data: ma21,
+                borderColor: "#FF8C00",
+                borderWidth: 1.5,
+                tension: 0.4,
+                pointRadius: 0,
+                fill: false
+            }
+            ]
         },
         options: {
             animation: {
-                duration: 1500,
-                easing: "easeInOutQuart"
+                duration: 600,
+                easing: "easeOutCubic"
+            },
+            transitions: {
+                show: {
+                    animations: {
+                        x: {duration: 0},
+                        y: {duration: 0}
+                    }
+                },
+                hide: {
+                    animations: {
+                        x: {duration: 0},
+                        y: {duration: 0}
+                    }
+                }
             },
             responsive: true,
             plugins: {
@@ -85,8 +150,205 @@ function renderChart(dates,prices,ma7,ma21,ticker){
                     grid: {
                         color: "rgba(255,255,255,0.1)"
                     }
+                },
+                yVolume: {
+                    position: "right",
+                    grid: {display:false},
+                    ticks: {display:false}
                 }
             }
         }
     });
+    updateMode();
+
+    const rsiCtx = document.getElementById("rsiChart").getContext("2d");
+
+    if (rsiChart) {
+        rsiChart.destroy();
+    }
+
+    rsiChart = new Chart(rsiCtx,{
+        type: "line",
+        data:{
+            labels: dates,
+            datasets: [{
+                label: "RSI (14)",
+                data: rsi,
+                borderColor: "#ff00ff",
+                borderWidth: 2,
+                tension: 0.3,
+                pointRadius: 0,
+                fill: false
+            },
+            {
+                label: "Overbought (70)",
+                data: new Array(dates.length).fill(70),
+                borderColor: "rgba(255,0,0,0.5)",
+                borderDash: [5,5],
+                pointRadius:0
+            },
+            {
+                label: "Oversold (30)",
+                data: new Array(dates.length).fill(30),
+                borderColor: "rgba(0,255,0,0.5)",
+                borderDash: [5,5],
+                pointRadius:0
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins:{
+                legend: {labels: {color: "white"}},
+                beforeDraw: (chart) => {
+                    const {ctx, chartArea} = chart;
+                    if (!chartArea) return;
+
+                    const {top,bottom,left,right} = chartArea;
+                    const height = bottom - top;
+
+                    ctx.save();
+
+                    ctx.fillStyle = "rgba(255,0,0,0.05)";
+                    ctx.fillRect(left,top,right - left,height * 0.3);
+
+                    ctx.fillStyle = "rgba(0,255,0,0.05)";
+                    ctx.fillRect(left,bottom - height * 0.3, right - left, height * 0.3);
+
+                    ctx.restore();
+                }
+            },
+            scales: {
+                y: {
+                    min: 0,
+                    max: 100,
+                    ticks: {color: "white"},
+                    grid: {color: "rgba(255,255,255,0.1)"},
+                    afterBuildTicks: function(scale) {
+                        scale.ticks = [0,30,50,70,100].map(v=>({value:v}));
+                    }
+                },
+                x: {display: false}
+            } 
+        }
+    });
+}
+
+function renderMACD(dates,macd,signal,hist){
+
+    const ctx = document.getElementById("macdChart").getContext("2d");
+    if (macdChart) macdChart.destroy();
+    macdChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: dates,
+            datasets: [
+                {
+                    type: "bar",
+                    label: "histogram",
+                    data: hist,
+                    backgroundColor: hist.map(v =>
+                        v >= 0 ? "rgba(0,255,0,0.6)" : "rgba(255,0,0,0.6)"
+                    )
+                },
+                {
+                    type: "line",
+                    label: "MACD",
+                    data: macd,
+                    borderColor: "#00ffff",
+                    borderWidth: 2,
+                    pointRadius: 0,
+                    tension: 0.4
+                },
+                {
+                    type: "line",
+                    label: "Signal",
+                    data: signal,
+                    borderColor: "#ffcc00",
+                    borderWidth: 2,
+                    pointRadius: 0,
+                    tension: 0.4
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {labels: {color: "white"}}
+            },
+            scales: {
+                x:{display:false},
+                y: {
+                    ticks: {color: "white"},
+                    grid: {color:"rgba(255,255,255,0.1)"}
+                }
+            }
+        }
+    });
+}
+
+document.getElementById("toggleMA7").addEventListener("change", function(){
+        chart.data.datasets[2].hidden = !this.checked;
+        if (!chart.data.datasets[2].hidden || !chart.data.datasets[3].hidden){
+            chart.data.datasets[1].borderWidth = 3;
+        } else {
+            chart.data.datasets[1].borderWidth = 4;
+        }
+        chart.update();
+    });
+document.getElementById("toggleMA21").addEventListener("change", function() {
+        chart.data.datasets[3].hidden = !this.checked;
+        if (!chart.data.datasets[2].hidden || !chart.data.datasets[3].hidden){
+            chart.data.datasets[1].borderWidth = 3;
+        } else {
+            chart.data.datasets[1].borderWidth = 4;
+        }
+        chart.update();
+    });
+
+let currentMode = "technical";
+
+const techBtn = document.getElementById("techMode");
+const aiBtn = document.getElementById("aiMode");
+
+function setmode(mode) {
+
+    currentMode = mode;
+
+    techBtn.classList.remove("active");
+    aiBtn.classList.remove("active");
+
+    if (mode === "technical"){
+        techBtn.classList.add("active");
+    } else {
+        aiBtn.classList.add("active");
+    }
+    updateMode();
+}
+
+techBtn.addEventListener("click", ()=> setmode("technical"));
+aiBtn.addEventListener("click", ()=> setmode("ai"));
+
+function updateMode() {
+
+    const techPanels = document.getElementById("technicalPanels");
+    const aiPanels = document.getElementById("aiPanels");
+
+    if (currentMode === "technical"){
+        techPanels.style.display = "block";
+        aiPanels.style.display = "none";
+    } else{
+        techPanels.style.display = "none";
+        aiPanels.style.display = "block";
+    }
+    if (!chart) return;
+
+    if (currentMode === "technical") {
+        chart.data.datasets[2].hidden = false; //for MA7
+        chart.data.datasets[3].hidden = false; //for MA21
+    } else{
+        chart.data.datasets[2].hidden = true;
+        chart.data.datasets[3].hidden = true;
+    }
+
+    chart.update();
 }
