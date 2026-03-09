ed620f74e8ff3382cc609cbbc0eb41994fb2d32d
RDC4321 <reenicawaling12@gmail.com>
2026-03-09 10:18:20 +0400

    Add files via upload
    Update: added new features and improvements to BroStock AI

--- services/stock_analyzer.py
+++ services/stock_analyzer.py
@@ -70,7 +70,8 @@ class StockAnalyzer:
         change = latest_price - previous_price
         percent_change = (change/previous_price) * 100
 
-        forecast_dates, forecast_prices = self.forecast_prices(days)
+        forecast_dates, forecast_prices, upper_band, lower_band = self.forecast_prices(days)
+        buy_points, sell_points, strong_buy_points, strong_sell_points = self.detect_signals()
 
         return{
             "ticker":self.ticker,
@@ -88,12 +89,21 @@ class StockAnalyzer:
             },
             "forecast": {
                 "dates": forecast_dates,
-                "prices": [safe_float(x) for x in forecast_prices]
+                "prices": [safe_float(x) for x in forecast_prices],
+                "upper_band": [safe_float(x) for x in upper_band],
+                "lower_band": [safe_float(x) for x in lower_band],
             },
             "change":safe_float(round(float(change),2)),
             "percent_change":safe_float(round(float(percent_change),2)),
+            "ai_signal": self.generate_signal(),
+            "signals":{
+                "buy":buy_points,
+                "sell": sell_points,
+                "strong_buy": strong_buy_points,
+                "strong_sell": strong_sell_points
+            }
         }
-    
+
     def forecast_prices(self, days_ahead=7):
         prices = np.array(self.close_prices).reshape(-1,1)
 
@@ -105,6 +115,10 @@ class StockAnalyzer:
         future_x = np.arange(len(prices), len(prices)+days_ahead).reshape(-1,1)
 
         forecast = model.predict(future_x)
+        residuals = prices.flatten() - model.predict(X).flatten()
+        std_dev = np.std(residuals)
+        upper_band = forecast.flatten() + (std_dev * 1.5)
+        lower_band = forecast.flatten() - (std_dev * 1.5)
 
         last_date = self.data.index[-1]
 
@@ -113,4 +127,118 @@ class StockAnalyzer:
             for i in range(days_ahead)
         ]
         
-        return forecast_dates, forecast.flatten()
+        return forecast_dates, forecast.flatten(), upper_band, lower_band
+
+    def generate_signal(self):
+
+        latest_rsi = self.rsi.iloc[-1]
+        latest_macd = self.macd.iloc[-1]
+        latest_signal = self.macd_signal.iloc[-1]
+        ma7 = self.ma7.iloc[-1]
+        ma21 = self.ma21.iloc[-1]
+
+        score = 0
+
+        # RSI momentum
+        if latest_rsi < 35:
+            score += 1
+        elif latest_rsi > 65:
+            score -= 1
+
+        # MACD trend
+        if latest_macd > latest_signal:
+            score += 1
+            macd_state = "Bullish Momentum"
+        else:
+            score -= 1
+            macd_state = "Bearish Momentum"
+
+        # Moving Average trend
+        if ma7 > ma21:
+            score += 1
+            trend = "Bullish"
+        else:
+            score -= 1
+            trend = "Bearish"
+
+        # Determine signal
+        if score >= 2:
+            signal = "STRONG BUY"
+        elif score == 1:
+            signal = "BUY"
+        elif score == 0:
+            signal = "HOLD"
+        elif score == -1:
+            signal = "SELL"
+        else:
+            signal = "STRONG SELL"
+
+        confidence = abs(score) * 33
+
+        return {
+            "signal": signal,
+            "confidence": confidence,
+            "trend": trend,
+            "rsi": round(float(latest_rsi), 2),
+            "macd": macd_state
+        }
+        
+    def detect_signals(self):
+        buy_points = []
+        sell_points = []
+        strong_buy_points = []
+        strong_sell_points = []
+
+        prices = self.close_prices.values
+        ma7 = self.ma7.values
+        ma21 = self.ma21.values
+        rsi = self.rsi.values
+        macd = self.macd.values
+        signal = self.macd_signal.values
+
+        for i in range(1, len(prices)):
+
+            score = 0
+            price = prices[i]
+
+            # TREND
+            prev_ma7 = ma7[i-1]
+            prev_ma21 = ma21[i-1]
+            curr_ma7 = ma7[i]
+            curr_ma21 = ma21[i]
+
+            # MOMENTUM
+            curr_rsi = rsi[i]
+
+            # MACD
+            prev_macd = macd[i-1]
+            prev_signal = signal[i-1]
+            curr_macd = macd[i]
+            curr_signal = signal[i]
+
+            if prev_ma7 < prev_ma21 and curr_ma7 > curr_ma21:
+                score += 30
+            if prev_ma7 > prev_ma21 and curr_ma7 < curr_ma21:
+                score -=30
+
+            if curr_rsi < 35:
+                score += 25
+            elif curr_rsi > 65:
+                score -= 25
+            
+            if prev_macd < prev_signal and curr_macd > curr_signal:
+                score += 20
+            if prev_macd > prev_signal and curr_macd < curr_signal:
+                score -= 20
+            
+            if score >= 50:
+                strong_buy_points.append((i,price))
+            elif score >= 25:
+                buy_points.append((i,price))
+            elif score <= -50:
+                strong_sell_points.append((i,price))
+            elif score <= -25:
+                sell_points.append((i,price))
+
+        return buy_points,sell_points, strong_buy_points, strong_sell_points
+        
